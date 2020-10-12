8a457296e1897726de63e543b857731866a3d2b4
Quentin <q.lemogodeuc@gmail.com>
2020-10-12 02:06:41 +0200

    chore: database client

--- app/database/index.js
+++ app/database/index.js
@@ -1,11 +1,9 @@
 const { Sequelize } = require("sequelize");
 
-const { DATABASE_PASSWORD, DATABASE_USER, DATABASE_HOST } = process.env;
+const { DATABASE_PASSWORD, DATABASE_USER } = process.env;
 
 const DATABASE_DB = process.env.NODE_ENV === "test" ? "onrewind_test" : process.env.DATABASE_DB;
-// console.log("DATABASE_DB", DATABASE_DB);
-// console.log("DATABASE_USER", DATABASE_USER);
-// console.log("DATABASE_PASSWORD", DATABASE_PASSWORD);
+
 const sequelize = new Sequelize(DATABASE_DB, DATABASE_USER, DATABASE_PASSWORD, {
   dialect: "postgres",
   host: "localhost",
@@ -15,21 +13,7 @@ const sequelize = new Sequelize(DATABASE_DB, DATABASE_USER, DATABASE_PASSWORD, {
     acquire: 30000,
     idle: 10000
   },
-  // sync: true,
   logging: false
 });
-
-// (async () => {
-//   try {
-//     if (process.env.NODE_ENV === "test") {
-//       console.log("Hitting _test DB");
-//       await sequelize.sync({ force: true, match: /_test$/ });
-//     } else {
-//       await sequelize.sync();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// })();
 
 module.exports = sequelize;
